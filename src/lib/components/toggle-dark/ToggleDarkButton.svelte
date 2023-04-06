af324a8dcd404d6db1088ab182ad6d1b178594ad
RavenP <ravenp.dev@gmail.com>
2023-04-06 11:17:02 +0800

    create button to toggle dark mode

---
+++ src/lib/components/toggle-dark/ToggleDarkButton.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import Icon from '@iconify/svelte';
+	import { darkMode } from '$lib/stores/darkMode';
+
+	function toggleDarkMode() {
+		darkMode.toggleDarkMode();
+
+		if ($darkMode && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+			document.documentElement.classList.add('dark');
+		} else {
+			document.documentElement.classList.remove('dark');
+		}
+	}
+</script>
+
+<button on:click={toggleDarkMode}>
+	<div
+		class="transition-all duration-500"
+		class:rotate-45={$darkMode}
+		class:-rotate-[135deg]={!$darkMode}
+	>
+		<!-- class:text-amber-500={!$darkMode} -->
+		<Icon icon="mdi:theme-light-dark" class="text-2xl" />
+	</div>
+</button>
