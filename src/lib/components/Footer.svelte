98369eb7ce78df630e7435049f9b6523df2bf15a
RavenP <ravenp.dev@gmail.com>
2023-07-28 04:14:27 +0800

    Create footer component

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,13 @@
+<footer class="max-w-md pb-16 text-sm text-slate-500 sm:pb-0">
+	<p>
+		Built with <a href="https://kit.svelte.dev/">Sveltekit</a> and
+		<a
+			class="font-medium text-slate-400 hover:text-teal-300 focus-visible:text-teal-300"
+			href="https://tailwindcss.com/">TailwindCSS</a
+		>. Heavily inspired by
+		<a
+			class="font-medium text-slate-400 hover:text-teal-300 focus-visible:text-teal-300"
+			href="https://brittanychiang.com/">Brittany Chiang</a
+		> website.
+	</p>
+</footer>
